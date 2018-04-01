164b68ba0d4da98f8a3355a769b3e6ec0a2719c8
Lars Jönsson <crymeat@me.com>
2018-04-01 11:30:46 +0200

    Added more pages

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,23 @@
 <template>
   <section class="section is-medium">
     <div class="container has-text-centered">
-      <h1 class="title">About Me</h1>
-      <p>This is the contact page</p>
+      <h1 class="title">Contact Me</h1>
+      <p>Do you like what you see?</p>
+      <form name="contact" method="POST" netlify>
+      <p>
+        <label>Your Name: <input type="text" name="name"></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email"></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+
     </div>
   </section>
 </template>
