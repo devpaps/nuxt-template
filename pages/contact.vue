de6ca6684affe6632bbebc5ce5c2b8918e5e9e70
Lars Jönsson <crymeat@me.com>
2018-04-01 11:27:43 +0200

    Added more pages

---
+++ pages/contact.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>This is the contact page</p>
+    </div>
+  </section>
+</template>
---
+++ pages/projects.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>This is the projects page</p>
+    </div>
+  </section>
+</template>
