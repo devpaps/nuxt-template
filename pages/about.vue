de8ad78f0339aa623136ca8658621655f705aba8
Lars Jönsson <crymeat@me.com>
2018-04-01 11:14:25 +0200

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>
+        My name is Chimezie Enyinnaya (AKA mezie), I’m a self taught software developer based in Lagos, Nigeria. I build modern applications for the web! I'm a technical writer, I write technical articles and tutorials for various platforms including <a href="https://scotch.io/@mezie">Scotch.io</a>.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+.about-header {
+  margin: 5rem 0;
+}
+</style>
